7226e78995bddf6f85931b248fc1ca9dfdc668b5
Lucas Johnson <A01199573@ad.bcit.ca>
2019-10-17 13:06:35 -0700

    C# console app

---
+++ Portfolio/Portfolio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Portfolio
+{
+    class Program
+    {
+        static class SqlHelper
+        {
+            public static SqlDataReader ExecuteReader(String connString, String commandText)
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    return reader;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            String connString = "Data Source=.; Initial Catalog=portfolio; Integrated Security=true;";
+            String commandText = "SELECT * FROM Project;";
+            using (SqlDataReader reader = SqlHelper.ExecuteReader(connString, commandText))
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["projectName"].ToString() + " " + reader["gitHubLink"].ToString());
+                }
+            }
+
+        }
+    }
+}
