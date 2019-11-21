512b4ebe9a9272266f1fd85b9a5e1f82528cd2d7
Lucas <woodsman.lucas@gmail.com>
2019-11-21 13:09:38 -0800

    commit to heroku

--- projects.php
+++ projects.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projects</title>
     <link rel="stylesheet" href="/styles/navbar.css">
+    <link rel="stylesheet" href="/styles/main.css">
     <link rel="import" href="/navbar.html">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Lato|Oswald&display=swap" rel="stylesheet">
@@ -18,22 +19,31 @@
         
 
         $conn=pg_pconnect("host=localhost dbname=" . dbname . " user=" . user . " password=" . password);
-        echo $conn;
-
 
         $result = pg_query($conn, "SELECT * FROM project");
+        
 
-        echo $result;
 
-        $row = pg_fetch_array($result);
-
-        echo $row
 
     ?>
 <div class="wrapper">
 <div id="header"></div>
  </div>
  <script src="scripts/menu.js"></script>   
+ <div class="cards">
+<?php
+        while ($row = pg_fetch_array($result)){
+        echo "<div class='card'>";
+        echo "<img src='$row[4]' alt='$row[1]'>";
+        echo "<div class='row'>";
+        echo $row[1]; // Name
+        echo "<a href='$row[2]'>$row[1] on github</a>"; // github link
+        echo "<a href='$row[3]'>$row[1] as a website</a>"; // on github pages
+        echo "</div></div>";
+        }
+?>
+</div>
+
 </body>
 <script>
         $(function(){
